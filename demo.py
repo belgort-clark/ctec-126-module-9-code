c63920378b3e527d210950fb5cfd47f6b4ffb48a
belgort-clark <belgort@clark.edu>
2020-06-01 14:27:10 -0700

    updates from class

---
+++ demo.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self, name, breed, color, gender, legs):
+        self.name = name
+        self.breed = breed
+        self.color = color
+        self.gender = gender
+        self.legs = legs
+        self.state = 'sitting'
+
+
+bruce = Dog('Bruce', 'English Springer Spaniel', 'brown', 'male', 4)
+
+print(bruce.name)
